c138d0ab00e2ed25c376b010a07b9de455930163
M.C. van den Boogaart <scola@kvdb.name>
2016-07-08 11:16:16 +0200

    Resolve events

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,33 @@
 import babel from 'rollup-plugin-babel';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+
+function resolveLocal() {
+  const map = {
+    events: require.resolve('events')
+  };
+
+  return {
+    resolveId: (importee) => {
+      return map[importee] ? map[importee] : null;
+    }
+  };
+}
 
 export default {
   entry: 'index.js',
   format: 'umd',
   globals: {
-    'd3-selection': 'd3',
-    'd3-dispatch': 'd3'
+    'd3-selection': 'd3'
   },
   plugins: [
+    resolve({
+      jsnext: true,
+      preferBuiltins: false,
+      skip: ['d3-selection']
+    }),
+    resolveLocal(),
+    commonjs(),
     babel({
       presets: ['es2015-rollup']
     })
